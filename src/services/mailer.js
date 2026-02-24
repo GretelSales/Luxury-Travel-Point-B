c51ca6f228974a450aa86c20b4d19418f9b995bf
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2026-02-23 22:22:10 -0500

    Service Interest added 7

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -3,12 +3,11 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  family: 4,
 });
 
 export const sendOwnerNotification = async ({
