d29cdd096b64f879b6c6a82449faf8dcea332e34
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2026-05-03 15:19:37 -0400

    delete the supabase anon key 2

--- src/db/supabaseClient.js
+++ src/db/supabaseClient.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 console.log(process.env.SUPABASE_URL);
-console.log(process.env.SUPABASE_ANON_KEY);
+console.log(process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
